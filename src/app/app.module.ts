1995e3919a3058b3a96caea1396e399531962dcc
stasDeveloper <87619975+stasyandikiy@users.noreply.github.com>
2022-12-07 19:44:25 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HeaderComponent } from './header/header.component';
+import { MomentPipe } from './shared/moment.pipe';
+import { TasksComponent } from './tasks/tasks.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AbsenceComponent } from './absence/absence.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    HeaderComponent,
+    TasksComponent,
+    MomentPipe,
+    AbsenceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatDatepickerModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
